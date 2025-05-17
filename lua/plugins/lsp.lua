d4d1d871bb0b44edf491e4d3f4dd0e20340473f3
naafidasana <ibrahimnaafi@gmail.com>
2025-05-17 17:38:01 +0200

    update cuda filetype to cu

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,7 +16,7 @@ return {
             "--fallback-style=Google",
             "--cross-file-rename",
           },
-          filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+          filetypes = { "c", "cpp", "objc", "objcpp", "cu" },
           root_dir = require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
         },
       },
